182e1bc7c5ce3dfc3810f43accaa689c31f01dde
Alex Delgado <delgadoalex@icloud.com>
2018-07-03 19:34:40 -0500

    Wrote if/else statements and appended buttons at random

--- js/app.js
+++ js/app.js
@@ -18,9 +18,9 @@ const printQuestion = () => {
   // console.log(Math.floor(Math.random() * questionsData.length));
   const index = Math.floor(Math.random() * questionsData.length);
 
-  console.log(questionsData[index].text);
-  console.log(questionsData[index]['a']);
-  console.log(questionsData[index]['correct']);
+  // console.log(questionsData[index].text);
+  // console.log(questionsData[index]['a']);
+  // console.log(questionsData[index]['correct']);
 
   // print answers
   const q = questionsData[index]; 
@@ -28,23 +28,24 @@ const printQuestion = () => {
     
     // if, else statement that prints everything but the answer // 
     // create event listeners 
-    
-    if(key !== 'correct') {
-      const $div = $('<div/>'); 
-      $div.attr('addClass', 'questions-div'); 
-      const $h1 = $('<h1/>');
-      $h1.text(q[key]); //this prints the value (kv pair)
-      $div.append($h1);
-      $('body').append($div);
+    // create the buttons based on the questions and answers. 
+      // make an event listener for each ty 
+    console.log(key)
+    if(key === 'text') {
+      const $h1 = $('<h1/>')
+      $h1.text(q[key])
+      $('body').append($h1);
+      console.log($h1)
+      console.log('text');
+
     }
-    $('.choice-1').on('click', (e) => {
-        console.log(key);
-        if('click' === '.choice-1'){
-          return true; 
-        }
-
-    })
-
+    else if(key !== 'correct'){
+      const $a = $('<button/>');
+      $a.text(q[key]); 
+      $('body').append($a); 
+       
+    }
+  
   }
   // console.log(key, 'key')
   // console.log(q[key]);
@@ -52,33 +53,11 @@ const printQuestion = () => {
 
 printQuestion();
 
-/// GAME FUNCTION /// 
 
-/// EVENT LISTENERS ///
+/// GAME OBJECT ///  
 
-// $('.choice-1').on('click', (e) => {
+// create game object that entails score, timer and event listeners. 
 
-//   // make selection for multiple choice 1 // 
-
-// });
-
-// $('.choice-2').on('click', (e) => {
-
-//   // make selection for multiple choice 2 //
-
-// }); 
-
-// $('.choice-3').on('click', (e) => {
-
-//   // make selection for multiple choice 3 // 
-  
-// });
-
-// $('.choice-4').on('click', (e) => {
-
-//   // make selection for multiple choice 4 // 
-
-// })
 
 
 
@@ -96,7 +75,7 @@ printQuestion();
 //   })
   
 
-// };
+// }; 360000
 
 
 
