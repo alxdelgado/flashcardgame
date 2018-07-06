3c43246fd0feea984ca18b11299c12ab4221f301
Alex Delgado <delgadoalex@icloud.com>
2018-07-06 09:49:13 -0500

    Add scoreboard and count score

--- js/app.js
+++ js/app.js
@@ -2,8 +2,7 @@ console.log('JS is working');
 
 /// GLOBAL SCOPE ///
 
-const gameTimer = 10;
-const gameScore = 8
+
 
 // window.alert('Enter Name');
 
@@ -14,69 +13,24 @@ const gameScore = 8
 
 // Function that prints a random question and multiple choice answers.  
 
-const printQuestion = (question) => {
-  // get random question from array
-  // print question and answers in html
-  // console.log(Math.floor(Math.random() * questionsData.length));
-
-  // make this a method in your game object
-  // const index = Math.floor(Math.random() * questionsData.length);
-
-  // console.log(questionsData[index].text);
-  // console.log(questionsData[index]['a']);
-  // console.log(questionsData[index]['correct']);
-
-  // print answers
-  const q = questionsData[index]; 
-  for (key in q) {
-    
-    // if, else statement that prints everything but the answer // 
-    // create event listeners 
-    // create the buttons based on the questions and answers. 
-      // make an event listener for each ty 
-    console.log(key)
-
-    // print the question
-    if(key === 'text') {
-      const $h1 = $('<h1/>')
-      $h1.text(q[key])
-      $('body').append($h1);
-      console.log($h1)
-      console.log('text');
-
-    }
-
-    // print the answer choices
-    else if(key !== 'correct'){
-      const $a = $('<button/>');
-      $a.text(q[key]); 
-      $('body').append($a); 
-       
-    }
-  
-  }
-
-  // console.log(key, 'key')
-  // console.log(q[key]) 
-};
-
-// printQuestion();
-
-
 // /// GAME OBJECT ///
 
 // // create a game object  
 
 const game = {
   score: 0,
-  time: 10, // set initial values when u declare
+  time: 0, // set initial values when u declare
+  timer: '', 
   removedQuestions: [], // initally this is empty
+  selectedAnswer: "",
+  correctAnswer: "",
   getRandomQuestion() {
     const index = Math.floor(Math.random() * questionsData.length);
-    console.log(index)
+    // console.log(index)
     // splice returns an array! https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/splice
+    this.correctAnswer = questionsData[index].correct
     const arrWithQuestion = questionsData.splice(index, 1);
-    console.log(arrWithQuestion)
+    // console.log(arrWithQuestion)
     const question = arrWithQuestion[0];
 
     this.removedQuestions.push(question)
@@ -93,6 +47,15 @@ const game = {
 
     const q = this.removedQuestions[this.removedQuestions.length-1]
     console.log(q)
+    const $submitButton = $('<button/>');
+    $submitButton.text('Submit Answer'); 
+    $('body').append($submitButton);
+    $submitButton.on('click', (e) => {
+      this.checkAnswer(); 
+      
+    
+    }); 
+
 
     for (key in q) {
     
@@ -100,35 +63,38 @@ const game = {
     // create event listeners 
     // create the buttons based on the questions and answers. 
       // make an event listener for each ty 
-      console.log(key)
+      // console.log(key)
 
       // prints the actualy text of the question
       if(key === 'text') {
         const $h1 = $('<h1/>')
         $h1.text(q[key])
         $('body').append($h1);
-        console.log($h1)
-        console.log('text');
+        // console.log($h1)
+        // console.log('text');
 
       }
 
       // appends buttons corresponding to each choice to the page
       else if(key !== 'correct'){
-        const $a = $('<button/>'); 
+        const $a = $('<button/>');  
         $a.text(q[key]);
         
         // add id to the button corresponding to the letter of the choice
-        $a.attr('id', 'multChoiceButtons');
+        $a.attr('class', 'multChoiceButtons');
+        $a.attr('id', key)
 
-        console.log($a); 
+        // console.log($a); 
 
+        // adds event listeners to each button 
         $a.on('click', (e) => {
-          console.log($('#multChoiceButtons')); 
-        }); 
-        
-        // add listener to this button
-        // see if you can get listener to console.log the id attached to it       
+          
 
+          this.selectedAnswer = e.currentTarget.id;
+
+          // console.log(this.selectedAnswer); 
+        }); 
+  
         $('body').append($a); 
 
       }
@@ -137,25 +103,60 @@ const game = {
     }  
 
 
+  },
+
+  checkAnswer(){
+    if(this.selectedAnswer === this.correctAnswer){
+      this.updateScore();
+      this.getRandomQuestion();
+      console.log(this.score); 
+      console.log('Correct Answer');  
+    }
+    else {
+      this.getRandomQuestion();
+      console.log('Wrong Answer')
+
+    }
+  }, 
+
+  updateScore(){
+
+    this.score++;
+    const $scoreBoard = $('#scoreBoard');
+    $scoreBoard.text(this.score);
+    console.log($scoreBoard); 
+
+
+  },
+
+  gameStart() {
+    // enter name to start game 
+    this.getRandomQuestion();
+    this.timer = setInterval(() => {
+      this.time++;
+      console.log(this.time);
+    }, 10)
+    
+
+
   }
 
 
 
 
 
-  // correct questions so far,
-  // property, 
-  // property
-  // doSomething() {
-      // this.score
-  // },
-
- 
 };
+$('#start_button').on('click', (e) => {
+  game.gameStart(); 
+  
 
-console.log(questionsData);
-game.getRandomQuestion();
-console.log(questionsData);
+})
+
+
+
+// console.log(questionsData);
+// game.getRandomQuestion();
+// console.log(questionsData);
 
 
 
@@ -186,22 +187,14 @@ console.log(questionsData);
 
     // and/or you could store that specific question in a property in the game object
 
-  // and then it will call printQuestion(q) and pass in the question that was randomly chosen. 
+  // and then it will call printQuestion(q) and pass in the question that was randomly chosen. - DONE 
 
 //   }
-
-
-
-// modify printQuestion to 
-// event listeners to answer buttons 
-
-
-
 
 // modify printQuestion to 
 // add id corresponding to answer choice to each button  when you're adding answer buttons
 // this will be used along with the current question that you are already tracking so that you can know which answer was chosen when user 
-// clicks a certiain button... which means you need 
+// clicks a certiain button... which means you need  - DONE 
 
 
 // method to check user answer -- this will be a method in game object
